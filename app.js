5e2f2cf984d2fc62ac866dd75452ecd506a35aed
mmmfang <mae.fang@gmail.com>
2015-11-22 15:18:26 -0500

    add npm packages

--- app.js
+++ app.js
@@ -5,19 +5,19 @@
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
 
-$('#getXML').click(function(e) {
-  $.ajax({
-   type: "GET",
-   dataType: "xml",
-   url: "http://web.mta.info/developers/data/nyct/nyct_ene.xml",
-   success: function(data){
-       console.log(data);
-       var data = data;
-    }, error: function(data) {
-     console.log("my bad, try again")
-   },
-  });
-});
+// $('#getXML').click(function(e) {
+//   $.ajax({
+//    type: "GET",
+//    dataType: "xml",
+//    url: "http://web.mta.info/developers/data/nyct/nyct_ene.xml",
+//    success: function(data){
+//        console.log(data);
+//        var data = data;
+//     }, error: function(data) {
+//      console.log("my bad, try again")
+//    },
+//   });
+// });
 
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
