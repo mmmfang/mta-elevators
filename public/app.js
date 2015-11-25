c52f14fd913e431f2ce9c730f541c280c9ca6ef6
mmmfang <mae.fang@gmail.com>
2015-11-25 15:13:07 -0500

    used converter and sucessfully changed xml to json

--- public/app.js
+++ public/app.js
@@ -5,135 +5,125 @@
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
 
-   $('#get-xml').click(function(e) {
-     $.ajax({
-      type: "GET",
-      dataType: "xml",
-      url: '/feed',
-      success: function(xml){
-       var data = xml; //all data - complete document is returned
-       //alert($(data).find("responsecode").text()); //this works! yay
-        $(data).find("outage").each(function(){
-          var singleOutage = this;
-        //console.log($(xml).text()); //this works too! yay, it gives me all info per outage
-       //console.log($(this).find("station").text()); this is working, printing all stations
-      $('#info-box').append('<p class="station">' + $(singleOutage).find("station").text() +
-        '</p><p class="whichTrain"> with the ' + $(singleOutage).find("trainno").text() +
-        ' trains</p><p class="boro">Borough: ' + $(singleOutage).find("borough").text() +
-        '</p><p class="whichExactly">Elevator or escalator?' + $(singleOutage).find("equipment").text() +
-        '</p><p class="whenReturning">Estimated Return to Service on: ' + $(singleOutage).find("estimatedreturntoservice").text() + '<hr />');
+  //  $('#get-xml').click(function(e) {
+  //    $.ajax({
+  //     type: "GET",
+  //     dataType: "xml",
+  //     url: '/feed',
+  //     success: function(xml){
+  //      var data = xml; //all data - complete document is returned
+  //      //alert($(data).find("responsecode").text()); //this works! yay
+  //       $(data).find("outage").each(function(){
+  //         var singleOutage = this;
+  //       //console.log($(xml).text()); //this works too! yay, it gives me all info per outage
+  //      //console.log($(this).find("station").text()); this is working, printing all stations
+  //     $('#info-box').append('<p class="station">' + $(singleOutage).find("station").text() +
+  //       '</p><p class="whichTrain"> with the ' + $(singleOutage).find("trainno").text() +
+  //       ' trains</p><p class="boro">Borough: ' + $(singleOutage).find("borough").text() +
+  //       '</p><p class="whichExactly">Elevator or escalator?' + $(singleOutage).find("equipment").text() +
+  //       '</p><p class="why">Reason? ' + $(singleOutage).find("reason").text() +
+  //       '</p><p class="whenReturning">Estimated Return to Service on: ' + $(singleOutage).find("estimatedreturntoservice").text() + 
+  //       '</p><p class="whenOutageStarted">Out of service since: ' + $(singleOutage).find("outagedate").text() +
+  //       '<hr />');
+  //       })
 
-        })
-     }, 
-      error: function(data) {
-        console.log("my bad, try again")
-     },
-    });
-  });
+  //    }, 
+  //     error: function(data) {
+  //       console.log("my bad, try again")
+  //    },
+  //   });
+  // });
 
- // $('ul').html('');
- //                if(data.Search !== undefined && data.Response !== "False"){
- //                  data.Search.forEach(function(item){
- //                    $('ul').append('<li>'+item.Title+'</li>');
- //                  });
- //                }
- //            },
+
 ///////////////////////////////////////////////////////////////////////
 ////??????????////////////ANGULAR TIME/////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
 
-//var app = angular.module('elevatorApp', []);
+var app = angular.module('elevatorApp', []);
 
-// app.controller('ElevatorController', ['$http', function($http) {
-//   var controller = this;
-//   $http.get('/feed').then(function (xml){
-//       alert("GOT XML")
-//         // console.log("xml is", xml);
-//     // function process(xml) {
-//     // $(xml).find("outage").each(function(){
-//     //   alert($(this).text());
-//       // if ($(this).find("station").text()="MN") {
-//     //     alert($(this).find("station").text())
-//     // 
-//     //   }
-//     // })
-//       // }
-//   //   }  
-//    })
-// }]);
+// THIS WORKS TOO
+// app.controller('ElevatorController', ['$http', function($http){
+//   var controller=this;
+//   this.makeAPICall = function(){
+//     var promise = $http.get('/feed');
+//     promise.success(function(data){
+//       controller.allData = data;
+//     $(data).find("outage").each(function(){
+//          var singleOutage = this;
+//         //console.log($(xml).text()); //this works too! yay, it gives me all info per outage
+//        //console.log($(this).find("station").text()); this is working, printing all stations
+//       $('#info-box').append('<p class="station">' + $(singleOutage).find("station").text() +
+//         '</p><p class="whichTrain"> with the ' + $(singleOutage).find("trainno").text() +
+//         ' trains</p><p class="boro">Borough: ' + $(singleOutage).find("borough").text() +
+//         '</p><p class="whichExactly">Elevator or escalator?' + $(singleOutage).find("equipment").text() +
+//         '</p><p class="why">Reason? ' + $(singleOutage).find("reason").text() +
+//         '</p><p class="whenReturning">Estimated Return to Service on: ' + $(singleOutage).find("estimatedreturntoservice").text() + 
+//         '</p><p class="whenOutageStarted">Out of service since: ' + $(singleOutage).find("outagedate").text() +
+//         '<hr />');
+//     })
+//   })
+// }
+// }])
+
+//TRYING TO USE THE XML2JSON converter but didnt work.. no errors but other errors include could be be instatiated
+app.controller('ElevatorController', ['$http', function($http){
+  var controller=this;
+  this.makeAPICall = function(){
+  $.get('/feed', function(xml){ 
+    var json = $.xml2json(xml); 
+    controller.outage = json.outage;
+    console.log(json); //all json
+    console.log(json.outage) //outages as objects in array 
+  });
+}
+}])
+
+
+
+
 
 // app.controller('ElevatorController', ['$http', function($http){
 //   var controller=this;
-//   $this.makeAPICall = function(){
-//     var promise = $http.get('/feed');
-//     promise.success(function(data){
-//       this.info = data;
-//       console.log(data);
+//   var allBoroughs = new Array();
+//   this.makeAPICall = function(){
+//     $http.get('/feed').success(function(data){
+//       controller.allData = data;
+//     $(data).find("outage").each(function(){
+//         allBoroughs.push($(this).find())
+//          var singleOutage = this;
+//         //console.log($(data).text()); //this works too! gives me all info per outage in giant text block
+//        var boroughs=($(singleOutage).find("borough").text()); //this is working, printing all stations
+//        console.log(boroughs)
+//       // $('#info-box').append('<p class="station">' + $(singleOutage).find("station").text() +
+//       //   '</p><p class="whichTrain"> with the ' + $(singleOutage).find("trainno").text() +
+//       //   ' trains</p><p class="boro">Borough: ' + $(singleOutage).find("borough").text() +
+//       //   '</p><p class="whichExactly">Elevator or escalator?' + $(singleOutage).find("equipment").text() +
+//       //   '</p><p class="why">Reason? ' + $(singleOutage).find("reason").text() +
+//       //   '</p><p class="whenReturning">Estimated Return to Service on: ' + $(singleOutage).find("estimatedreturntoservice").text() + 
+//       //   '</p><p class="whenOutageStarted">Out of service since: ' + $(singleOutage).find("outagedate").text() +
+//       //   '<hr />');
 //     })
-//   }
+//   }).error(function(err){
+//     console.log('error making xml call')
+//   })
+// }
 // }])
 
+        // .controller("MyController", function($scope, $http) {
+        //     $scope.myData = {};
+        //     $scope.myData.doClick = function(item, event) {
 
-    
-  
+        //         var responsePromise = $http.get("/angularjs-examples/json-test-data.jsp");
 
-
-// $.get('data/animals.xml', function(xml){
-// var animals = $.xml2json(xml);
-// alert(animals.dog[1].name +'/'+ animals.dog[1]);
-// });
-
-// function xmlParser(xml)) {
-//     $('#load').fadeOut();
-//     $(xml).find("Book").each(function () {
-//         $(".main").append('<div class="book"><div class="title">' + 
-//  $(this).find("Title").text() + '</div><div class="description">' + 
-//  $(this).find("Description").text() + '</div><div class="date">Published ' + 
-//  $(this).find("Date").text() + '</div></div>');
-//         $(".book").fadeIn(1000);
-
-//     });
-
-// }
-
-// $('Contact',xml).each(function() {
-//   srno = parseInt($(this).find("srno").text());
-//   empId = $(this).find("empid").text();
-//   name = $(this).find("name").text();
-//   contact = $(this).find("contact-data").text();
-//   type = $(this).find("type").text();
-// })
+        //         responsePromise.success(function(data, status, headers, config) {
+        //             $scope.myData.fromServer = data.title;
+        //         });
+        //         responsePromise.error(function(data, status, headers, config) {
+        //             alert("AJAX failed!");
+        //         });
+        //     }
 
 
-// +  
-// +  $.each(json.???rss.channel.item???, function(i, outage){
-// +    $('#info-box').append('<p>'
-// +      '<strong>'+outage.station+'</strong><br/>'
-// +      '<u>Borough</u>: '+outage.borough+'<br/>'
-// +      '<u>Back to Service</u>: '+outage.estimatedreturntoservice+'<br/>'
-// + 
-// +    '</p>');
-// +  });
-// +});
-
-//  $('#getXML').click(function(e) {
-//      $.ajax({
-//     type: "GET",
-//     dataType: "xml",
-//     url: '/feed',
- 
-//     success: function(data){ //data returns everything 
-
-//       console.log(data);
-//       var data = data;
-//         // $(data).find("outage").each(function(){
-//        //  $("#info-box").append($(data).find("station").text() + "<br />")
-//        }, error: function(data) {
-//         console.log("my bad, try again");
-//       }
-//    });
-//   });
-// })
 
 // insdie of server.js u make a requset to /feed ..... 
 // npm request is a npm that matt uses in OAUTH.. so the server can make the request, but the angular cannot
--- server.js
+++ server.js
@@ -1,27 +1,42 @@
-var express 	= require ('express'),
-	server		= express(),
-	request		= require('request'),
-//	PORT		= process.env.PORT || 5432,
-	MONGOURI 	= process.env.MONGOLAB_URI || "mongodb://localhost:27017/mta_elevator",
-	dbname		= "mta_elevator",
-	ejs 		= require('ejs'),
-	bodyParser 	= require('body-parser'),
-	methodOverride = require('method-override'),
-	expressLayouts = require('express-ejs-layouts'),
-	session 	= require('express-session'),
-	morgan		=require('morgan'),
-	mongoose	= require('mongoose'),
-	Schema 		= mongoose.Schema;
+var express 		= require ('express'),
+	server			= express(),
+	request			= require('request'),
+	PORT			= process.env.PORT || 5432,
+	MONGOURI 		= process.env.MONGOLAB_URI || 'mongodb://localhost:27017',
+	dbname			= 'mta-elevator',
+	ejs 			= require('ejs'),
+	bodyParser 		= require('body-parser'),
+	methodOverride 	= require('method-override'),
+	expressLayouts 	= require('express-ejs-layouts'),
+	session 		= require('express-session'),
+	morgan			= require('morgan'),
+	mongoose		= require('mongoose'),
+	Schema 			= mongoose.Schema;
 
+
+//var parser = require('xml2json');
+
+function ensureAuthenticated(req,res,next) {
+	if (req.session.username) {
+		next()
+	} else {
+		res.redirect('/')
+	}
+} 
 
 // Set view engine 
 server.set('views', './views');
 server.set('view engine', 'ejs');
 
-
 server.use(express.static('./public'));
 server.use(expressLayouts);
+server.use(morgan('dev'));
+server.use(methodOverride('_method'));
 
+server.use(bodyParser.urlencoded({
+	extended:true
+}));
+server.use(bodyParser.json()); //i guess this lets me get json
 
 //Testing Route
 server.get('/test', function(req,res){
@@ -33,30 +48,79 @@ server.get('/', function(req,res) {
 	res.render('index');
 });
 
-
-//using request npm 
+//using request npm sample
 	// var grabAPI = request('http://web.mta.info/developers/data/nyct/nyct_ene.xml', function (error, response, body) {
 	//   if (!error && response.statusCode == 200) {
 	//     //console.log(body); // Show the MTA data. 
 	//     
 	// })
 
-
- 
 server.use('/feed', function(req, res) {  
   req.pipe(request('http://web.mta.info/developers/data/nyct/nyct_ene.xml')).pipe(res);
 });
 
+//let's say i saved things like data in the db. id get it by in my controllers/users.js, after ive required router, router.post(function(req,res)) --to create a new user  / router.get(function(req,res{User.find(function(err, users){ if (err)res.send(err); res.json(users);});});  to get all the Users
 
-// server.post('/feed', function(response) {
+//Get a users/user:id
+//router.get(function(req,res){
+// 	User.findById(req.params.user_id, function (err, user){
+// 		if (err) 
+// 			res.send(err);
+// 		res.json(bear);
+// 	})
+// })
 
-// 	grabAPI(response);
+//Get Routes Time
+// server.get('/', home)
+// server.post('/login', home)
+// server.all('/', ensureAuthenticated)
 
-// });
 
+//======MONGOOSE SCHEMAS - shove this in models/user.js  
+// var mongoose = require('mongoose'),  
+// Schema = mongoose.Schema;
+
+// var userSchema = Schema({
+// 	email: {type: String, required: true, unique: true}, //how to verify its an email, can i do that in chrome with the user validation
+// 	password: {type: String, required: true}
+// }, {collection: 'users', strict:false});
+
+// var User = mongoose.model("User", userSchema);
+// //storing user documents in a collection called users
+// module.exports = User;
+
+//=======Model - store in models/post.js
+// var mongoose = require('mongoose'),
+// 	Schema = mongoose.Schema;
+
+// var preferenceSchema = Schema({
+// 	station: {type: String},
+// 	trainline: {type: String},
+//	alert: {type: boolean},
+// 	body: {type: String, required: true},
+
+// }, {collection: 'preferences', strict:false});
+
+///HOW do i do the belongs to association for the user in mongo or node 
+
+// var Post = mongoose.model("Post", postSchema);
+
+// module.exports = Post;
+/////=====END OF MONGOOSE SCHEMAS TO PASTE IN LATER
+
+
+//Mongoose starts
+mongoose.set('debug', true);
 
 //connecting to Mongo database
-mongoose.connect('mongodb://localhost:27017/mta_elevator');
-server.listen(3000, function(){
-  console.log("Server is up at 3000");
+mongoose.connect(MONGOURI + "/" + dbname);
+
+var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function callback() {
+            console.log('db connection open');
+        });
+        
+server.listen(PORT, function() {
+	console.log("SERVER IS UP ON PORT", PORT);
 });
